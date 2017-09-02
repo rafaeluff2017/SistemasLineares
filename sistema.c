92bd6784d295006c4f54dec490797da3eb760fc9
Rafael Ribeiro <rmatheus@id.uff.br>
2017-09-02 16:18:38 -0300

    Imprimindo matriz de Gauss-Jordan

--- sistema.c
+++ sistema.c
@@ -30,3 +30,12 @@ main()
             }
         }
     }
+			//imprir matriz diagonalizado
+     	for (i = 1; i <= (n+1); i++) {
+  		for (j = 1; j <= (n+1); j++) {
+    			printf("%f\t", A[i][j]);
+
+  			}
+		printf("\n");
+	}
+
