4aa4e48908136dd02be10db9e4eb69e16063446a
Rafael Ribeiro <rmatheus@id.uff.br>
2017-09-02 16:16:23 -0300

    Lendo a matriz aumentada

--- sistema.c
+++ sistema.c
@@ -4,4 +4,14 @@ main()
 	int i,j,k,n;				//Declaração de variavie
 	float A[20][20],c,x[10];
 	printf("\nEntre com o tamanho da matriz: ");
+	scanf("%d",&n);
+	printf("\nEntre com os elementos da matriz aumentada:\n");
+	for(i=1; i<=n; i++)
+   	{
+		for(j=1; j<=(n+1); j++)
+		{
+            		printf(" A[%d][%d]:", i,j);
+            		scanf("%f",&A[i][j]);
+      		}
+   	}
 
